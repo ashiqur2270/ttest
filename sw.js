1cbe85e2f24c27d55bbf73aafaf81cbb7d4194ae
Engineer Ashik <ashiqur2270@gmail.com>
2026-01-09 18:42:11 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,27 @@
-// Service Worker রেজিস্ট্রেশন
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('sw.js')
-      .then(reg => console.log('Service Worker নিবন্ধিত হয়েছে!'))
-      .catch(err => console.log('Service Worker রেজিস্ট্রেশনে সমস্যা:', err));
-  });
-}
+const CACHE_NAME = 'mydash-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/1041/1041916.png'
+];
 
+// ফাইলগুলো ক্যাশ (Cache) এ জমা রাখা
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// অফলাইনে ফাইলগুলো ব্রাউজার থেকে না খুঁজে ক্যাশ থেকে নেওয়া
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
